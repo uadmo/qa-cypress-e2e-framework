e3ffa003bc2c8f913e74277c8512f41d1b602923
uadmo <61236161+uadmo@users.noreply.github.com>
2026-01-13 16:45:38 +0000

    Add negative login scenarios with typed fixtures

--- cypress/e2e/login/login.cy.ts
+++ cypress/e2e/login/login.cy.ts
@@ -1,10 +1,40 @@
 import LoginPage from '@pages/LoginPage'
+import { User } from '../../types/user'
+
+beforeEach(() => {
+    LoginPage.visit()
+})
 
 describe('Login', () => {
     it('Should login successfully', () => {
-        LoginPage.visit()
         LoginPage.login('standard_user', 'secret_sauce')
 
         cy.url().should('include', '/inventory')
+    })
+})
+
+describe ('Login - invalid scenarios', () => {
+    let validUser: User
+    let invalidUser: User
+    let lockedUser: User
+
+    before(() => {
+        cy.fixture('users').then((users) => {
+            validUser = users.validUser
+            invalidUser = users.invalidUser
+            lockedUser = users.lockedUser
+        })
+    })
+
+    it('Should not login with invalid credentials', () => {
+        LoginPage.login(invalidUser.username, invalidUser.password)
+
+        LoginPage.errorMessage().should('contain', 'Epic sadface')
+    })
+
+    it('Should not login with locked out user', () => {
+        LoginPage.login(lockedUser.username, lockedUser.password)
+
+        LoginPage.errorMessage().should('contain', 'locked out')
     })
 })
--- cypress/pages/LoginPage.ts
+++ cypress/pages/LoginPage.ts
@@ -8,6 +8,10 @@ class LoginPage {
         cy.get('#password').type(password)
         cy.get('#login-button').click()
     }
+
+    errorMessage(): Cypress.Chainable<string> {
+        return cy.get('[data-test="error"]').invoke('text')
+    }
 }
 
 export default new LoginPage()
